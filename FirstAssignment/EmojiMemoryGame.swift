ff95f7699012af2642e9ce0901faffebfa9e92b9
hoon3284 <hoon3284@icloud.com>
2021-02-05 02:50:04 +0900

    Commit like demo

---
+++ FirstAssignment/EmojiMemoryGame.swift
@@ -0,0 +1,27 @@
+//
+//  EmojiMemoryGame.swift
+//  FirstAssignment
+//
+//  Created by wickedRun on 2021/02/05.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
---
+++ FirstAssignment/MemoryGame.swift
@@ -0,0 +1,32 @@
+//
+//  MemoryGame.swift
+//  FirstAssignment
+//
+//  Created by wickedRun on 2021/02/05.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
