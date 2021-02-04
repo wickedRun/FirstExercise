c489fa5a0d1a21d0590a34583f7b0d8ea58322ad
hoon3284 <hoon3284@icloud.com>
2021-02-04 19:33:32 +0900

    Initial Commit

---
+++ FirstAssignment/FirstAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAssignmentApp.swift
+//  FirstAssignment
+//
+//  Created by wickedRun on 2021/02/04.
+//
+
+import SwiftUI
+
+@main
+struct FirstAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
